8750041a189d2e73446fdf95dac190a913c37e10
Evante <evantegl@gmail.com>
2012-09-28 18:38:31 +0200

    navbar shows active page

--- notebook/notebook/application/handlers/site.py
+++ notebook/notebook/application/handlers/site.py
@@ -2,7 +2,7 @@ import os
 import jinja2
 import logging
 
-jinja_environment = jinja2.Environment(
+env = jinja2.Environment(
 	loader = jinja2.FileSystemLoader('templates')) # os.path.dirname(__file__)
 
 import webapp2
@@ -14,66 +14,65 @@ from google.appengine.api import users, mail
 class Splash(webapp2.RequestHandler):
 	def get(self):
 
-		_user = users.get_current_user()
-		if _user:
+		title="Home"
+
+		user = users.get_current_user()
+		if user:
 			url = users.create_logout_url(self.request.uri)
 		else:
 			url = users.create_login_url(self.request.uri)
 
 		template_values = {
-			'user': _user,
-			'login_url': url
+			'user': user,
+			'login_url': url,
+			'title': title,
 		}
 
-		template = jinja_environment.get_template('splash.jinja')
+		template = env.get_template('splash.jinja')
 		self.response.out.write(template.render(template_values))
 
 class About(webapp2.RequestHandler):
 	def get(self):
 
-		_user = users.get_current_user()
-		if _user:
+		title="About"
+
+		user = users.get_current_user()
+		if user:
 			url = users.create_logout_url(self.request.uri)
 		else:
 			url = users.create_login_url(self.request.uri)
 
 		template_values = {
-			'user': _user,
-			'login_url': url
+			'user': user,
+			'login_url': url,
+			'title': title,
 		}
 
-		template = jinja_environment.get_template('about.jinja')
+		template = env.get_template('about.jinja')
 		self.response.out.write(template.render(template_values))
 
 class Contact(webapp2.RequestHandler):
 	def get(self):
+		
+		title="Contact"
 
-		_user = users.get_current_user()
-		if _user:
+		user = users.get_current_user()
+		if user:
 			url = users.create_logout_url(self.request.uri)
 		else:
 			url = users.create_login_url(self.request.uri)
 
 		template_values = {
-			'user': _user,
-			'login_url': url
+			'user': user,
+			'login_url': url,
+			'title': title,
 		}
 
-		template = jinja_environment.get_template('contact.jinja')
+		template = env.get_template('contact.jinja')
 		self.response.out.write(template.render(template_values))
 
 	def post(self):
 		user = users.get_current_user()
-		# if _user:
-		# 	url = users.create_logout_url(self.request.uri)
-		# else:
-		# 	url = users.create_login_url(self.request.uri)
-
-		# template_values = {
-		# 	'user': _user,
-		# 	'login_url': url
-		# }
-		# template=jinja_environment.get_template('contact.jinja')
 
 		_name = self.request.get('name')
 		_message = self.request.get('message')
